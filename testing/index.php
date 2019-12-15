33d2d174686c3199efc47f4db8442c95509913bb
Juan Treminio <jtreminio@gmail.com>
2019-12-14 22:37:14 -0600

    Overhauls testing library; automates building and pushing

--- testing/index.php
+++ testing/index.php
@@ -1,1 +1,19 @@
-<?php phpinfo();
+<?php
+
+header_remove('X-Powered-By');
+header('Content-Type: text/html');
+header_remove('Content-Type');
+
+$setting = $_GET['setting'] ?? null;
+
+if ($setting === 'version') {
+    echo phpversion();
+    exit;
+}
+
+if ($setting) {
+    echo ini_get($setting);
+    exit;
+}
+
+phpinfo();
