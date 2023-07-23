fdad76c297e3176c44e3ffceb863ef2171b05875
emirlike <projectmanager@emirlike.com>
2023-07-22 20:22:50 -0700

    small improvements

--- files/php/bin_install_composer.sh
+++ files/php/bin_install_composer.sh
@@ -10,3 +10,6 @@ curl -sS https://getcomposer.org/installer | \
     php -- --install-dir=/usr/local/bin \
            --filename=composer &&\
 chown -R www:www /.composer
+
+
+# curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --version=1.10.16 
